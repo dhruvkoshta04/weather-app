c22acbb51f040dcde76b043549264f7f3d9f15ca
DhruvKoshta <dhruvkoshta@gmail.com>
2022-09-22 21:16:48 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3a8004e20fmshe87aef8f6104e72p1670f3jsn6fd642ee2b07',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/"
+
+export const WEATHER_API_KEY = "fedd0639668e871a31c69bede6c70a16"
